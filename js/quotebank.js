e9dff25e2b332f23e31ab4be05a50a7fc5c603b7
trily84 <51871742+trily84@users.noreply.github.com>
2020-11-14 15:54:14 -0700

    Add files via upload

---
+++ js/quotebank.js
@@ -0,0 +1,31 @@
+const QUOTEBANK = [
+      {
+        quote: "You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.",
+        author: "Dr. Suess"
+      },
+      {
+        quote: "Get busy living or get busy dying.",
+        author: "Larry King"
+      },
+      {
+        quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
+        author: "Mark Twain"
+      },
+      {
+        quote: "Those who dare to fail miserably can achieve greatly.",
+        author: "John F. Kennedy"
+      },
+      {
+        quote: "So many books, so little time.",
+        author: "Frank Zappa"
+      },
+      {
+        quote: "Be the change that you wish to see in the world.",
+        author: "Mahatma Gandhi"
+      },
+      {
+        quote: "Always forgive your enemies; nothing annoys them so much.",
+        author: "Oscar Wilde"
+      }
+    
+    ];
